16a2595b4f929d04930b0b2b7989d9f5599041c4
Josh <josh.haddix598@gmail.com>
2022-09-27 19:21:51 -0500

    integrated webpack

---
+++ client/src/js/index.js
@@ -0,0 +1,22 @@
+// import modules
+import "./form";
+import "./submit";
+
+// import CSS files
+import "../css/index.css";
+
+// import Images
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
+
+// Add bootstrap
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// add images on load
+window.addEventListener("load", function () {
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
+});
