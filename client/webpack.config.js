8e87b7926fe3ff29e34d0ff85789eb58059e5e7a
Josh <josh.haddix598@gmail.com>
2022-09-29 16:14:19 -0500

    created sw file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,6 +10,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./index.html",
       title: "Webpack Plugin",
+    }),
+    new InjectManifest({
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js",
     }),
   ],
   output: {
